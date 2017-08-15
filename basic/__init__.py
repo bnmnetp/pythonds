31e2021d741093298533cf3da76755c72f24b955
Roman Yasinovskyy <yasiro01@luther.edu>
2017-08-15 15:30:10 -0500

    Added linked lists to basic data structures __init__.py

--- basic/__init__.py
+++ basic/__init__.py
@@ -4,5 +4,7 @@ pythonds.basic import statement
 from .stack import Stack
 from .queue import Queue
 from .deque import Deque
+from .linked_list import OrderedList
+from .linked_list import UnorderedList
 
-__all__ = ["Stack", "Queue", "Deque"]
+__all__ = ["Stack", "Queue", "Deque", "OrderedList", "UnorderedList"]
--- test_basic_init.py
+++ test_basic_init.py
@@ -16,18 +16,24 @@ class TestBasicsInit(unittest.TestCase):
         self._stack = Stack()
         self._queue = Queue()
         self._deque = Deque()
+        self._olist = OrderedList()
+        self._ulist = UnorderedList()
 
     def test_is_empty(self):
         '''Testing is_empty() method'''
         self.assertTrue(self._stack.is_empty())
         self.assertTrue(self._queue.is_empty())
         self.assertTrue(self._deque.is_empty())
+        self.assertTrue(self._olist.is_empty())
+        self.assertTrue(self._ulist.is_empty())
 
     def tearDown(self):
         '''Tearing down'''
         del self._stack
         del self._queue
         del self._deque
+        del self._olist
+        del self._ulist
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
