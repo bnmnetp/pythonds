63655074a4a11762b74fc9d85022ecb396d95510
Roman Yasinovskyy <yasiro01@luther.edu>
2017-08-15 21:14:11 -0500

    Added graph algorithms

--- graphs/adjacency_graph.py
+++ graphs/adjacency_graph.py
@@ -5,50 +5,8 @@ Copyright 2005
 Updated by Roman Yasinovskyy, 2017
 '''
 
+import heapq
 import sys
-
-
-class Graph:
-    '''Graph as an adjacency matrix'''
-    def __init__(self):
-        self._vertices = {}
-        self._time = 0
-
-    def get_vertex(self, key):
-        '''Find the vertex in the graph named vert_key'''
-        return self._vertices.get(key, None)
-
-    def set_vertex(self, key):
-        '''Add an instance of Vertex to the graph'''
-        self._vertices[key] = Vertex(key)
-
-    def __contains__(self, key):
-        '''in operator override'''
-        return key in self._vertices
-
-    def add_edge(self, from_vertex, to_vertex, weight=0):
-        '''Add a weighted and directed edge to the graph'''
-        if from_vertex not in self._vertices:
-            self.set_vertex(from_vertex)
-        if to_vertex not in self._vertices:
-            self.set_vertex(to_vertex)
-        self._vertices[from_vertex].set_neighbor(self._vertices[to_vertex], weight)
-
-    def get_vertices(self):
-        '''Return the list of all vertices in the graph'''
-        return self._vertices.keys()
-
-    def __iter__(self):
-        '''Iterator'''
-        return iter(self._vertices.values())
-
-    def size(self):
-        '''Graph's size'''
-        return len(self._vertices)
-
-    def __len__(self):
-        '''Graph's size'''
-        return len(self._vertices)
 
 
 class Vertex:
@@ -62,6 +20,10 @@ class Vertex:
         self._previous = None
         self._discovery_time = 0
         self._closing_time = 0
+
+    def __lt__(self, other):
+        '''Less than operator required for heapify'''
+        return self.key < other.key
 
     def get_key(self):
         '''Get vertex key'''
@@ -109,7 +71,7 @@ class Vertex:
         '''Set previous'''
         self._previous = previous
 
-    previoua = property(get_previous, set_previous)
+    previous = property(get_previous, set_previous)
 
     def get_discovery_time(self):
         '''Get discovery time'''
@@ -138,3 +100,118 @@ class Vertex:
                 ":closing_time " + str(self._closing_time) + \
                 ":distance " + str(self._distance) + \
                 ":previous \n\t[" + str(self._previous) + "]\n"
+
+
+class Graph:
+    '''Graph as an adjacency matrix'''
+    def __init__(self):
+        self._vertices = {}
+        self._time = 0
+
+    def __iter__(self):
+        '''Iterator'''
+        return iter(self._vertices.values())
+
+    def size(self):
+        '''Graph's size'''
+        return len(self._vertices)
+
+    def __len__(self):
+        '''Graph's size'''
+        return len(self._vertices)
+
+    def __contains__(self, key):
+        '''in operator override'''
+        return key in self._vertices
+
+    def get_vertex(self, key):
+        '''Find the vertex in the graph named vert_key'''
+        return self._vertices.get(key, None)
+
+    def set_vertex(self, key):
+        '''Add an instance of Vertex to the graph'''
+        self._vertices[key] = Vertex(key)
+
+    def add_edge(self, from_vertex, to_vertex, weight=0):
+        '''Add a weighted and directed edge to the graph'''
+        if from_vertex not in self._vertices:
+            self.set_vertex(from_vertex)
+        if to_vertex not in self._vertices:
+            self.set_vertex(to_vertex)
+        self._vertices[from_vertex].set_neighbor(self._vertices[to_vertex], weight)
+
+    def get_vertices(self):
+        '''Return the list of all vertices in the graph'''
+        return self._vertices.keys()
+
+    def reset_distances(self):
+        '''Reset distances to test Dijkstra's'''
+        for vertex in self:
+            vertex.set_distance(sys.maxsize)
+
+    def bfs(self, start):
+        '''Breadth First Search'''
+        start.set_distance(0)
+        start.set_previous(None)
+        vert_queue = [start]
+        while vert_queue:
+            current_vert = vert_queue.pop(0)
+            for neigh in current_vert.get_neighbors():
+                if neigh.get_color() == 'white':
+                    neigh.set_color('gray')
+                    neigh.set_distance(current_vert.get_distance() + 1)
+                    neigh.set_previous(current_vert)
+                    vert_queue.append(neigh)
+            current_vert.set_color('black')
+
+    def dfs(self):
+        '''Depth First search'''
+        for vertex in self:
+            if vertex.get_color() == 'white':
+                self.dfs_visit(vertex)
+
+    def dfs_visit(self, start):
+        '''DFS helper function'''
+        start.set_color('gray')
+        self._time = self._time + 1
+        start.set_discovery_time(self._time)
+        for next_vertex in start.get_neighbors():
+            if next_vertex.get_color() == 'white':
+                next_vertex.set_previous(start)
+                self.dfs_visit(next_vertex)
+        start.set_color('black')
+        self._time = self._time + 1
+        start.set_closing_time(self._time)
+
+    def traverse(self, src, dst):
+        '''Traverse a graph'''
+        path = []
+        current = self.get_vertex(dst)
+        while current:
+            path.insert(0, current)
+            current = current.get_previous()
+        print('Path from {} to {} ({}): {}'.format(self.get_vertex(src).get_key(),
+                                                   self.get_vertex(dst).get_key(),
+                                                   self.get_vertex(dst).get_distance(),
+                                                   ' '.join(v.get_key() for v in path)))
+
+    def dijkstra(self, start):
+        '''Dijkstra's shortest path algorithm'''
+        start.set_distance(0)
+        not_yet_visited = [[start.get_distance(), start]]
+        heapq.heapify(not_yet_visited)
+        while not_yet_visited:
+            current_vertex = heapq.heappop(not_yet_visited)[1]
+            for next_vertex in current_vertex.get_neighbors():
+                new_distance = current_vertex.get_distance() + current_vertex.get_neighbor(next_vertex)
+                if new_distance < next_vertex.get_distance():
+                    next_vertex.set_distance(new_distance)
+                    next_vertex.set_previous(current_vertex)
+                    found = False
+                    for vertex in not_yet_visited:
+                        if vertex[1].get_key() == next_vertex.get_key():
+                            vertex[0] = next_vertex.get_distance()
+                            heapq.heapify(not_yet_visited)
+                            found = True
+                    if not found:
+                        heapq.heappush(not_yet_visited, [next_vertex.get_distance(), next_vertex])
--- graphs/test_adjacency_graph.py
+++ graphs/test_adjacency_graph.py
@@ -39,6 +39,72 @@ class TestAdjacencyGraphMethods(unittest.TestCase):
         self.assertEqual(sorted(output.getvalue().strip().split()),
                          ['t', 'u', 'v', 'w', 'x', 'y', 'z'])
 
+    def test_dijkstra(self):
+        '''Testing Dijkstra's shortest path algorithm'''
+        expected_result_from_t = {'u': [['t', 'u'], 2],
+                                  'v': [['t', 'v'], 4],
+                                  'w': [['t', 'u', 'w'], 5],
+                                  'x': [['t', 'v', 'x'], 7],
+                                  'y': [['t', 'y'], 7],
+                                  'z': [['t', 'v', 'x', 'z'], 15]
+                                 }
+        self._graph.reset_distances()
+        start_vertex = 't'
+        self._graph.dijkstra(self._graph.get_vertex(start_vertex))
+        for vertex in self._graph.get_vertices() - {start_vertex}:
+            with patch('sys.stdout', new=StringIO()) as output:
+                self._graph.traverse(start_vertex, vertex)
+            self.assertEqual(output.getvalue().strip(),
+                             'Path from {} to {} ({}): {}'.format(start_vertex,
+                                                                  vertex,
+                                                                  expected_result_from_t[vertex][1],
+                                                                  ' '.join(expected_result_from_t[vertex][0])))
+
+    def test_bfs(self):
+        '''Testing Breadth first search'''
+        expected_result_from_t = {'u': [['t', 'u'], 1],
+                                  'v': [['t', 'v'], 1],
+                                  'w': [['t', 'u', 'w'], 2],
+                                  'x': [['t', 'y', 'x'], 2],
+                                  'y': [['t', 'y'], 1],
+                                  'z': [['t', 'y', 'z'], 2]
+                                 }
+        start_vertex = 't'
+        self._graph.bfs(self._graph.get_vertex(start_vertex))
+        for vertex in self._graph.get_vertices() - {start_vertex}:
+            with patch('sys.stdout', new=StringIO()) as output:
+                self._graph.traverse(start_vertex, vertex)
+            self.assertEqual(output.getvalue().strip(),
+                             'Path from {} to {} ({}): {}'.format(start_vertex,
+                                                                  vertex,
+                                                                  expected_result_from_t[vertex][1],
+                                                                  ' '.join(expected_result_from_t[vertex][0])))
+
+    def test_dfs(self):
+        '''Testing Depth first search'''
+        expected_result_from_t = {'t': (1, 14),
+                                  'u': (2, 13),
+                                  'v': (7, 8),
+                                  'w': (3, 12),
+                                  'x': (4, 11),
+                                  'y': (6, 9),
+                                  'z': (5, 10)
+                                 }
+        start_vertex = 't'
+        self._graph.dfs_visit(self._graph.get_vertex(start_vertex))
+        for vertex in self._graph:
+            if vertex.get_color() == 'white':
+                self._graph.dfs_visit(vertex)
+        for vertex in self._graph.get_vertices():
+            with patch('sys.stdout', new=StringIO()) as output:
+                print('Time of {} is {}/{}'.format(self._graph.get_vertex(vertex).get_key(),
+                                                   self._graph.get_vertex(vertex).get_discovery_time(),
+                                                   self._graph.get_vertex(vertex).get_closing_time()))
+            self.assertEqual(output.getvalue().strip(),
+                             'Time of {} is {}/{}'.format(vertex,
+                                                          expected_result_from_t[vertex][0],
+                                                          expected_result_from_t[vertex][1]))
+
     def tearDown(self):
         '''Tearing down'''
         del self._graph
