d55312abcbea4a5a3532d91e2a2d80b1f93361f3
Roman Yasinovskyy <yasiro01@luther.edu>
2018-06-19 14:41:47 -0500

    Prepare for packaging

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setuptools.setup(
     description="Data Structures package for Problem Solving with Algorithms and Data Structures using Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yasinovskyy/pythonds",
+    url="https://github.com/yasinovskyy/pythonds3",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+) (GPL)",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ),
 )
