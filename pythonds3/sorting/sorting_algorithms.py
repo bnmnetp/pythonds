534dcc0e90cff495811299f368007531e3845efe
Roman Yasinovskyy <yasinovskyy@gmail.com>
2020-01-26 10:21:31 -0600

    Add exchange condition to Bubble sort

--- pythonds3/sorting/sorting_algorithms.py
+++ pythonds3/sorting/sorting_algorithms.py
@@ -12,9 +12,13 @@ import heapq
 def bubble_sort(lst):
     """Bubble sort"""
     for i in range(len(lst) - 1, 0, -1):
+        exchanges = False
         for j in range(i):
             if lst[j] > lst[j + 1]:
+                exchanges = True
                 lst[j], lst[j + 1] = lst[j + 1], lst[j]
+        if not exchanges:
+            break
 
 
 def select_sort(lst):
