1855eea708af1ea796df3e5264639b0c827e3ee5
Roman Yasinovskyy <yasinovskyy@gmail.com>
2021-12-17 11:00:18 -0600

    Preparing to release 3.1.0

--- setup.py
+++ setup.py
@@ -1,21 +1,32 @@
+"""Package setup"""
+
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="pythonds3",
-    version="3.0.3",
+    version="3.1.0",
     author="Roman Yasinovskyy",
     author_email="yasinovskyy@gmail.com",
     description="Data Structures package for Problem Solving with Algorithms and Data Structures using Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yasinovskyy/pythonds3",
+    url="https://github.com/psads/pythonds3",
     packages=setuptools.find_packages(),
     license="GPLv3+",
     platforms=["OS Independent"],
-    keywords = ["Education", "Algorithms", "Data Structures", "Python", "Stack", "Queue", "Tree", "Graph"],
+    keywords=[
+        "Education",
+        "Algorithms",
+        "Data Structures",
+        "Python",
+        "Stack",
+        "Queue",
+        "Tree",
+        "Graph",
+    ],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
