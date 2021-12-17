31a22415dd7dbd81df7fa1d3e282b3a79c3317ee
Roman Yasinovskyy <yasinovskyy@gmail.com>
2021-12-17 09:30:37 -0600

    Add type annotations

--- pythonds3/basic/stack.py
+++ pythonds3/basic/stack.py
@@ -7,29 +7,32 @@ Updated by Roman Yasinovskyy, 2017
 """
 
 
+from typing import Any
+
+
 class Stack:
-    """Stack implementation as a list"""
+    """Stack implementation using a list"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Create new stack"""
-        self._items = []
+        self._items: list[Any] = []
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         """Check if the stack is empty"""
         return not bool(self._items)
 
-    def push(self, item):
+    def push(self, item: Any) -> None:
         """Add an item to the stack"""
         self._items.append(item)
 
-    def pop(self):
+    def pop(self) -> Any:
         """Remove an item from the stack"""
         return self._items.pop()
 
-    def peek(self):
+    def peek(self) -> Any:
         """Get the value of the top item in the stack"""
         return self._items[-1]
 
-    def size(self):
+    def size(self) -> int:
         """Get the number of items in the stack"""
         return len(self._items)
