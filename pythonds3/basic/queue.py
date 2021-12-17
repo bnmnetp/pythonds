3151c5001d8f920ab7b9fd0d6f0e74b3317afada
Roman Yasinovskyy <yasinovskyy@gmail.com>
2021-12-17 09:28:58 -0600

    Add type annotations

--- pythonds3/basic/queue.py
+++ pythonds3/basic/queue.py
@@ -7,25 +7,28 @@ Updated by Roman Yasinovskyy, 2017
 """
 
 
+from typing import Any
+
+
 class Queue:
-    """Queue implementation as a list"""
+    """Queue implementation using a list"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Create new queue"""
-        self._items = []
+        self._items: list[Any] = []
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         """Check if the queue is empty"""
         return not bool(self._items)
 
-    def enqueue(self, item):
+    def enqueue(self, item: Any) -> None:
         """Add an item to the queue"""
         self._items.insert(0, item)
 
-    def dequeue(self):
+    def dequeue(self) -> Any:
         """Remove an item from the queue"""
         return self._items.pop()
 
-    def size(self):
+    def size(self) -> int:
         """Get the number of items in the queue"""
         return len(self._items)
