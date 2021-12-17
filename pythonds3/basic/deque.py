72b5f89b05b044fb0fd6c8f2968f8ec17198f20e
Roman Yasinovskyy <yasinovskyy@gmail.com>
2021-12-17 09:11:44 -0600

    Add type annotations

--- pythonds3/basic/deque.py
+++ pythonds3/basic/deque.py
@@ -7,8 +7,11 @@ Updated by Roman Yasinovskyy, 2017
 """
 
 
+from typing import Any
+
+
 class Deque:
-    """Queue implementation as a list"""
+    """Dequeue implementation using a list"""
 
     def __init__(self):
         """Create new deque"""
@@ -18,22 +21,22 @@ class Deque:
         """Check if the deque is empty"""
         return not bool(self._items)
 
-    def add_front(self, item):
+    def add_front(self, item: Any):
         """Add an item to the front of the deque"""
         self._items.append(item)
 
-    def add_rear(self, item):
+    def add_rear(self, item: Any):
         """Add an item to the rear of the deque"""
         self._items.insert(0, item)
 
-    def remove_front(self):
+    def remove_front(self) -> Any:
         """Remove an item from the front of the deque"""
         return self._items.pop()
 
-    def remove_rear(self):
+    def remove_rear(self) -> Any:
         """Remove an item from the rear of the deque"""
         return self._items.pop(0)
 
-    def size(self):
+    def size(self) -> int:
         """Get the number of items in the deque"""
         return len(self._items)
