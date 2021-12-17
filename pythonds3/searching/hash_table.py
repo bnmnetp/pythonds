cbe3d62cea59729ee50db891c8c9dc26c720788b
Roman Yasinovskyy <yasinovskyy@gmail.com>
2021-12-17 09:51:32 -0600

    Add type annotations

--- pythonds3/searching/hash_table.py
+++ pythonds3/searching/hash_table.py
@@ -7,48 +7,51 @@ Updated by Roman Yasinovskyy, 2017
 """
 
 
+from typing import Any, Union
+
+
 class HashTable:
     """Hash Table implementation"""
 
-    def __init__(self, size=16):
+    def __init__(self, size=16) -> None:
         """Create a hash table"""
         self._size = size
-        self._slots = [None] * self._size
-        self._data = [None] * self._size
+        self._slots: list[Union[int, None]] = [None] * self._size
+        self._data: list[Union[Any, None]] = [None] * self._size
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: int) -> Any:
         """Magic __get__"""
         return self.get(key)
 
-    def __setitem__(self, key, data):
+    def __setitem__(self, key: int, data: Any) -> None:
         """Magic __set__"""
         self.put(key, data)
 
-    def __len__(self):
+    def __len__(self) -> int:
         """Magic __len__"""
         return self._size - self._slots.count(None)
 
-    def __contains__(self, key):
-        """Magin in"""
+    def __contains__(self, key: int) -> bool:
+        """Magic in"""
         return key in self._slots
 
-    def _hash_function(self, key, size):
+    def _hash_function(self, key: int, size: int) -> int:
         """Simple hash function"""
         return key % size
 
-    def _rehash(self, old_hash, size, step=1):
+    def _rehash(self, old_hash: int, size: int, step: int = 1) -> int:
         """Simple rehash function"""
         return (old_hash + step) % size
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         """Check if the table is empty"""
         return self._size == self._slots.count(None)
 
-    def size(self):
+    def size(self) -> int:
         """Get number of items in the table"""
         return self._size - self._slots.count(None)
 
-    def put(self, key, data):
+    def put(self, key: int, data: Any) -> None:
         """Add an item to the table"""
         hash_value = self._hash_function(key, len(self._slots))
 
@@ -73,11 +76,11 @@ class HashTable:
                     self._slots[next_slot] = key
                     self._data[next_slot] = data
                 elif j == self._size:
-                    raise Exception("Hash Table is full")
+                    raise MemoryError("Hash Table is full")
                 else:
                     self._data[next_slot] = data  # replace
 
-    def get(self, key):
+    def get(self, key: int) -> Any:
         """Get an item from the table"""
         start_slot = self._hash_function(key, len(self._slots))
         position = start_slot
@@ -89,4 +92,4 @@ class HashTable:
             j = j + 1
             position = self._rehash(start_slot, len(self._slots), j)
 
-        raise KeyError("{} is not in the table".format(key))
+        raise KeyError(f"{key} is not in the table")
