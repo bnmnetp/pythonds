1d36801825a429f009985b2dc2253f475097d675
Roman Yasinovskyy <yasinovskyy@gmail.com>
2021-12-17 09:43:39 -0600

    Add type annotations

--- pythonds3/intro/oop.py
+++ pythonds3/intro/oop.py
@@ -7,64 +7,58 @@ Updated by Roman Yasinovskyy, 2017
 """
 
 
-def gcd(number1, number2):
-    """Helper function to simplify fractions"""
-    if not isinstance(number1, int) or not isinstance(number2, int):
-        raise TypeError("Numerator and denominator must be integers")
-    while number1 % number2:
-        number1, number2 = number2, number1 % number2
-    return number2
+from math import gcd
 
 
 class Fraction:
     """Fraction class"""
 
-    def __init__(self, new_numer, new_denom):
-        try:
-            common = gcd(new_numer, new_denom)
-        except TypeError:
-            raise
+    def __init__(self, new_numer: int, new_denom: int) -> None:
+        """Create new fraction"""
+        if not isinstance(new_numer, int) or not isinstance(new_denom, int):
+            raise TypeError("Numerator and denominator must be integers")
+        common = gcd(new_numer, new_denom)
         self._numer = new_numer // common
         self._denom = new_denom // common
 
     @property
-    def numer(self):
+    def numer(self) -> int:
         """Get numerator"""
         return self._numer
 
     @numer.setter
-    def set_numer(self, new_numer):
+    def numer(self, new_numer: int) -> None:
         """Set numerator"""
         self._numer = new_numer
 
-    def get_denom(self):
+    def get_denom(self) -> int:
         """Get deniminator"""
         return self._denom
 
-    def set_denom(self, new_denom):
+    def set_denom(self, new_denom: int) -> None:
         """Set deniminator"""
         self._denom = new_denom
 
     # Another way to create an attribute
     denom = property(get_denom, set_denom)
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Get the string value"""
         if self._numer > self._denom:
-            return "{} {}/{}".format(
-                self._numer // self._denom, self._numer % self._denom, self._denom
-            )
-        return "{}/{}".format(self._numer, self._denom)
+            return f"{self._numer // self._denom} {self._numer % self._denom}/{self._denom}"
+        return f"{self._numer}/{self._denom}"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Fraction representation"""
-        return "Fraction({}, {})".format(self._numer, self._denom)
+        return f"Fraction({self._numer}, {self._denom})"
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         """Equality comparison"""
+        if not isinstance(other, Fraction):
+            return NotImplemented
         return self._numer * other.denom == other.numer * self._denom
 
-    def __add__(self, other):
+    def __add__(self, other: "Fraction") -> "Fraction":
         """Add two fractions"""
         new_numer = self._numer * other.denom + self._denom * other.numer
         new_denom = self._denom * other.denom
